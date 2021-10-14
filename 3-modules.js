89d54fa57907919e88debb33465551ecb20d8ef7
Atik-Mujawar <atikmujawar92@gmail.com>
2021-10-14 20:23:31 +0530

    initial

---
+++ 3-modules.js
@@ -0,0 +1,32 @@
+// const amount = 9
+// if (amount < 10){
+//     console.log('number is smaller than 10');
+// }else{
+//     console.log('number is larger than 10');
+// }
+
+// console.log('hey this is my first app')
+
+//GLOBALS
+// console.log(__dirname)
+
+// setInterval(() => {
+//     console.log('hello world')
+// }, 1000);
+
+//Modules
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
+
+// console.log(sayHi('atik'))
+// console.log(names)
+
+const unclebob = require('./6-alternative-flavour')
+// console.log(unclebob)
+
+require('./7-mind-grenade')
